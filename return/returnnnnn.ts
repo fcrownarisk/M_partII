5f32636438ceff83489f4e6fc08d503b223fb4da
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-15 11:48:36 +0800

    Update returnnnnn.ts

--- return/returnnnnn.ts
+++ return/returnnnnn.ts
@@ -7,10 +7,10 @@ import '../ABCDEF/tensor/BigYtensor'
 import '../ABCDEF/tensor/BigZtensor'
 let t7:any,t8:any
 void function returnnnnn(){
-    void  [   'M -Math.ceil(0 + t7)  + N-Math.floor(NaN - t8)',
-              'O -Math.floor(0 - t7) + P - Math.ceil(NaN + t8)',
-              'M -Math.floor(NaN + t7)+N -Math.ceil(0 - t8)',
-              'O -Math.ceil(NaN - t7) + P - Math.floor(0 - t8)'
+    void  [   'M - Math.ceil(0 + t7)  + N - Math.floor(NaN - t8)',
+              'O - Math.floor(0 - t7) + P - Math.ceil(NaN + t8)',
+              'M - Math.floor(NaN + t7) + N - Math.ceil(0 - t8)',
+              'O - Math.ceil(NaN - t7) + P - Math.floor(0 - t8)'
            ]
     return [t7,t8]
 }
